cdf0841c4e4f21b1cfa83695dbc6a20fb169ea28
techinn0vation <techinnovation64@gmail.com>
2023-05-26 10:29:24 -0300

    Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,27 @@
-import { BlockFooter, ContentFooter, WrapperFooter } from './styles'
+import { BlockFooter, ContentFooter, LinkFooter, WrapperFooter } from './styles'
+import DisplayTitle from '../Title'
+
+import Logo from '..//..//..//public/img/Logo.png'
+import { LogoBranding } from '../Navigation/styles'
 
 export default function Footer() {
   return (
     <WrapperFooter>
       <ContentFooter>
-        <BlockFooter></BlockFooter>
+        <BlockFooter>
+          <DisplayTitle DisplayTitle="interesses" />
+          <LinkFooter href="">sobre nós</LinkFooter>
+          <LinkFooter href="">categorias</LinkFooter>
+          <LinkFooter href="">trabalhe conosco</LinkFooter>
+        </BlockFooter>
+        <BlockFooter>
+          <LogoBranding src={Logo} alt="Mão na roda Logo" priority={true} />
+          <LinkFooter href="">política de privacidade</LinkFooter>
+          <LinkFooter href="">termos de uso</LinkFooter>
+        </BlockFooter>
+        <BlockFooter>
+          <DisplayTitle DisplayTitle="siga-nos nas redes sociais" />
+        </BlockFooter>
       </ContentFooter>
     </WrapperFooter>
   )
--- src/components/Footer/styles.tsx
+++ src/components/Footer/styles.tsx
@@ -1,5 +1,46 @@
+import Link from 'next/link'
 import styled from 'styled-components'
 
-export const WrapperFooter = styled.footer``
-export const ContentFooter = styled.div``
-export const BlockFooter = styled.div``
+export const WrapperFooter = styled.footer`
+  width: 100%;
+  height: auto;
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-template-rows: auto;
+  justify-content: center;
+  align-items: center;
+`
+export const ContentFooter = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+`
+export const BlockFooter = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+
+  h1 {
+    color: ${props => props.theme.colors.colorA};
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.4rem;
+    line-height: 2.1rem;
+    text-transform: uppercase;
+  }
+`
+export const LinkFooter = styled(Link)`
+  color: ${props => props.theme.colors.colorB};
+  font-style: normal;
+  font-weight: 600;
+  font-size: 1.4rem;
+  line-height: 2.1rem;
+  text-decoration: none;
+  text-transform: uppercase;
+`
