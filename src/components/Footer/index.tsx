ef903d77c14cdc16843b01b1e0cc04a1a01a2171
techinn0vation <techinnovation64@gmail.com>
2023-05-27 12:47:56 -0300

    Update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,21 @@
-import { BlockFooter, ContentFooter, LinkFooter, WrapperFooter } from './styles'
+import {
+  BlockFooter,
+  ContentFooter,
+  FooterLineEnd,
+  LinkFooter,
+  SocialField,
+  WrapperFooter,
+  WrapperSocialField
+} from './styles'
+
 import DisplayTitle from '../Title'
+import DisplayTypography from '../Typography'
 
 import Logo from '..//..//..//public/img/Logo.png'
 import { LogoBranding } from '../Navigation/styles'
+
+import { FiFacebook, FiInstagram } from 'react-icons/fi'
+import { AiOutlineMail, AiOutlineWhatsApp } from 'react-icons/ai'
 
 export default function Footer() {
   return (
@@ -10,19 +23,36 @@ export default function Footer() {
       <ContentFooter>
         <BlockFooter>
           <DisplayTitle DisplayTitle="interesses" />
-          <LinkFooter href="">sobre nós</LinkFooter>
-          <LinkFooter href="">categorias</LinkFooter>
-          <LinkFooter href="">trabalhe conosco</LinkFooter>
+          <LinkFooter href="/sobreNos">sobre nós</LinkFooter>
+          <LinkFooter href="#">categorias</LinkFooter>
+          <LinkFooter href="#">trabalhe conosco</LinkFooter>
         </BlockFooter>
         <BlockFooter>
           <LogoBranding src={Logo} alt="Mão na roda Logo" priority={true} />
-          <LinkFooter href="">política de privacidade</LinkFooter>
-          <LinkFooter href="">termos de uso</LinkFooter>
+          <LinkFooter href="#">política de privacidade</LinkFooter>
+          <LinkFooter href="#">termos de uso</LinkFooter>
         </BlockFooter>
         <BlockFooter>
           <DisplayTitle DisplayTitle="siga-nos nas redes sociais" />
+          <WrapperSocialField>
+            <SocialField href="#">
+              <FiFacebook />
+            </SocialField>
+            <SocialField href="mailto:contato@mao-na-roda.com">
+              <AiOutlineMail />
+            </SocialField>
+            <SocialField href="https://wa.me/5541996108063?text=Ol%C3%A1%2C+vim+do+site+%F0%9F%98%80+%3A">
+              <AiOutlineWhatsApp />
+            </SocialField>
+            <SocialField href="https://www.instagram.com/maonaroda_oficiall/">
+              <FiInstagram />
+            </SocialField>
+          </WrapperSocialField>
         </BlockFooter>
       </ContentFooter>
+      <FooterLineEnd>
+        <DisplayTypography DisplayTypography="© 2023, Mão na Roda. CNPJ 49.856.962/0001-36 - Todos os direitos reservados." />
+      </FooterLineEnd>
     </WrapperFooter>
   )
 }
--- src/components/Footer/styles.tsx
+++ src/components/Footer/styles.tsx
@@ -3,12 +3,20 @@ import styled from 'styled-components'
 
 export const WrapperFooter = styled.footer`
   width: 100%;
-  height: auto;
   display: grid;
   grid-template-columns: repeat(1, 1fr);
   grid-template-rows: auto;
   justify-content: center;
   align-items: center;
+  gap: 2rem;
+
+  &::before {
+    content: '';
+    width: 100%;
+    height: 0.05rem;
+    background: ${props => props.theme.colors.colorA};
+    opacity: 0.66;
+  }
 `
 export const ContentFooter = styled.div`
   width: 100%;
@@ -17,6 +25,11 @@ export const ContentFooter = styled.div`
   justify-content: center;
   align-items: center;
   gap: 2rem;
+
+  /* Configuração de Media MD (Medium)*/
+  @media (min-width: ${props => props.theme.screenSize.sizeMD}) {
+    flex-direction: row;
+  }
 `
 export const BlockFooter = styled.div`
   width: 100%;
@@ -24,15 +37,19 @@ export const BlockFooter = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  text-align: center;
   gap: 1rem;
 
+  /* Component DisplayTitle */
   h1 {
     color: ${props => props.theme.colors.colorA};
     font-style: normal;
     font-weight: 600;
     font-size: 1.4rem;
     line-height: 2.1rem;
+    opacity: 0.88;
     text-transform: uppercase;
+    cursor: default;
   }
 `
 export const LinkFooter = styled(Link)`
@@ -43,4 +60,87 @@ export const LinkFooter = styled(Link)`
   line-height: 2.1rem;
   text-decoration: none;
   text-transform: uppercase;
+  cursor: pointer;
+`
+export const WrapperSocialField = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`
+export const SocialField = styled(Link)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  color: ${props => props.theme.colors.colorA};
+  font-size: 2rem;
+  border: solid ${props => props.theme.colors.colorA} 0.2rem;
+  border-radius: 100%;
+  padding: 0.5rem;
+
+  transition: ease-in 0.09s;
+  text-decoration: none;
+
+  &:focus:active {
+    transform: translateY(0.2rem);
+  }
+
+  /* Animações em  Mídias Sociais*/
+  &:nth-child(1):hover {
+    transition: ease-in 0.09s;
+    -webkit-transition: ease-in 0.09s;
+    -o-transition: ease-in 0.09s;
+    -moz-transition: ease-in 0.09s;
+    color: ${props => props.theme.colors.colorB};
+    border: solid ${props => props.theme.colors.colorB} 0.2rem;
+    box-shadow: 0 0 0.6rem 0 ${props => props.theme.colors.colorB};
+  }
+  &:nth-child(2):hover {
+    transition: ease-in 0.09s;
+    -webkit-transition: ease-in 0.09s;
+    -o-transition: ease-in 0.09s;
+    -moz-transition: ease-in 0.09s;
+    color: ${props => props.theme.colors.colorE};
+    border: solid ${props => props.theme.colors.colorE} 0.2rem;
+    box-shadow: 0 0 0.6rem 0 ${props => props.theme.colors.colorE};
+  }
+  &:nth-child(3):hover {
+    transition: ease-in 0.09s;
+    -webkit-transition: ease-in 0.09s;
+    -o-transition: ease-in 0.09s;
+    -moz-transition: ease-in 0.09s;
+    color: ${props => props.theme.colors.colorH};
+    border: solid ${props => props.theme.colors.colorH} 0.2rem;
+    box-shadow: 0 0 0.6rem 0 ${props => props.theme.colors.colorH};
+  }
+  &:nth-child(4):hover {
+    transition: ease-in 0.09s;
+    -webkit-transition: ease-in 0.09s;
+    -o-transition: ease-in 0.09s;
+    -moz-transition: ease-in 0.09s;
+    color: ${props => props.theme.colors.colorI};
+    border: solid ${props => props.theme.colors.colorI} 0.2rem;
+    box-shadow: 0 0 0.6rem 0 ${props => props.theme.colors.colorI};
+  }
+`
+export const FooterLineEnd = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  p {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.4rem;
+    line-height: 2.1rem;
+    color: ${props => props.theme.colors.colorA};
+    text-align: center;
+    opacity: 0.88;
+  }
 `
